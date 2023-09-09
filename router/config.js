042cb61d3ad78b1c53e793865dfe8ab2757f6dbc
Debraj Mukherjee <debraj@sapientive.com>
2023-09-09 14:20:22 +0530

    login app server first commit

---
+++ router/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET: "4mkrNRFOnP2zk338iNLfSMZPKSSLYJ7crnVXRwGDtEI=",
+    EMAIL: "ivah.rice@ethereal.email",
+    PASSWORD: "BWESbcVMnB1hahzwyr",
+    ATLAS_URI: "mongodb+srv://mukherjeedebraj07:u85PbCZiKEEzCTIj@cluster0.leymdxy.mongodb.net/login_auth?retryWrites=true&w=majority"
+}
